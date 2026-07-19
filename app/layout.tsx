5ecd8d4d0d596e150c71bf151114ae5884b5d1a0
NguyenKhangPhuc <nkpnkpnkp2005@gmail.com>
2026-07-20 02:06:17 +0300

    Fix: fix logo problem

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/assets/search_logo.png" />
         <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
       </head>
       <LoaderProvider>
